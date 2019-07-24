891f78c5417730b45f73b0974e65e8857b4712c4
Luis F. Hercules <fernando_hercules9@hotmail.com>
2019-07-23 23:28:16 -0600

    agregamos una lista de productos

---
+++ SistemaDeNotas/SistemaDeNotas.Web/Controllers/AlumnosController.cs
@@ -0,0 +1,34 @@
+﻿using SistemaDeNotas.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaDeNotas.Web.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            var alumno1 = new AlumnoModel();
+            alumno1.Id = 1;
+            alumno1.Nombre = "Luis Hercules";
+
+            var alumno2 = new AlumnoModel();
+            alumno2.Id = 2;
+            alumno2.Nombre = "Danny Nuñez";
+
+            var alumno3 = new AlumnoModel();
+            alumno3.Id = 3;
+            alumno3.Nombre = "Cristian ";
+
+            var listadeAlumnos = new List<AlumnoModel>();
+            listadeAlumnos.Add(alumno1);
+            listadeAlumnos.Add(alumno2);
+            listadeAlumnos.Add(alumno3);
+            return View(listadeAlumnos);
+        }
+    }
+}
---
+++ SistemaDeNotas/SistemaDeNotas.Web/Models/AlumnoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaDeNotas.Web.Models
+{
+    public class AlumnoModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
